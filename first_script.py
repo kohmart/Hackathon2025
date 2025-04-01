813411a0bf28595f397997280b0ea903215e15c5
Martin <martin.kohlik@tul.cz>
2025-04-01 14:48:12 +0200

    Updated first script for our web

--- first_script.py
+++ first_script.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 
-driver = webdriver.Edge()
+driver = webdriver.Chrome()
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+driver.get("http://cloudalm.cz")
 
 title = driver.title
 print(title)
